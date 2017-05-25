72f1417c69c6990f0406367e7b34db7227ee81c0
Nate Hart <HartBeatNT@gmail.com>
2017-05-25 15:56:27 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: [path.resolve(__dirname, "dev/app.js")],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, "dev"),
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  watch: true,
+};
